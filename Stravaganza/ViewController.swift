f69fac72904b6424929d6a9dee99c988d385cb09
Marcos Varani <marcos.varani@mercadolibre.com>
2022-10-07 15:29:34 -0300

    Initial project setup

---
+++ Stravaganza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stravaganza
+//
+//  Created by Marcos Federico Varani on 07/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
